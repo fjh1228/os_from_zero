22ccaa28d4c91809ed54dc6b4115af2e8d47cd37
fjh1228 <2468307821@qq.com>
2023-09-04 19:44:53 +0800

    实现了本章的全部功能

--- kernel/main.c
+++ kernel/main.c
@@ -7,7 +7,13 @@ int main(){
 
     init_all();
 
+    int i = 0;
     asm volatile ("sti");   //演示中断，在这里临时开中断
+    for(i == 0; i < 50000; i++){
+        if(i >= 40000){
+            asm volatile ("cli");
+        }
+    }
     put_str("program is now!\n");
     while(1);
 
